1cdec37d71b7d4a71a49cbb635fcf744400421d0
dani88cala <daniele.calabrese@hotmail.it>
2025-03-15 15:44:13 +0100

    fix folder name

--- 3 - Data Prepocessing/Datapreprocessing.py
+++ 3 - Data Prepocessing/Datapreprocessing.py
@@ -2,9 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.impute import SimpleImputer
 
 # Importing the dataset
-dataset = pd.read_csv('3 - DataPrepocessing/Data.csv')
+dataset = pd.read_csv('3 - Data Prepocessing/Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print("Indipendet variables: ", X)
